8f4b0305b4c7d12435431d1b4c1399d287bbc6d6
GitHub Action <action@github.com>
2025-05-28 18:38:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/tke/compositefixplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tke/compositefixplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tke.compositefixplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
